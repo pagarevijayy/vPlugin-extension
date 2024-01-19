14e70c2aa899b7c588887d35116f665f32ccaa31
pagarevijayy <pagarevj97@gmail.com>
2024-01-19 15:59:43 +0530

    enhanced update color

--- src/components/update-page-bg.jsx
+++ src/components/update-page-bg.jsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
+
 const UpdatePageBG = () => {
+    let [color, setColor] = useState("#4287f5");
+
     const modifyPageDOM = async () => {
-        let [tab] = await chrome.tabs.query({
+        let [tab] = await chrome.tabs?.query({
             active: true,
             currentWindow: true,
         });
 
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: () => {
-                document.body.style.backgroundColor = "rgb(138,180,248)";
+            args: [color],
+            func: (color) => {
+                document.body.style.backgroundColor = color;
             },
         });
     };
 
     return (
         <div>
+            <input
+                style={{ margin: "4px" }}
+                type="color"
+                value={color}
+                onChange={(e) => {
+                    setColor(e.target.value);
+                }}
+            />
             <button
                 onClick={() => {
                     modifyPageDOM();
